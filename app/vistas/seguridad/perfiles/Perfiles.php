0b52ea2fb7bd08018a7549d0dcfad7bb9d60ab79
JorgeAO <jaguilar.8709@gmail.com>
2020-11-11 19:19:05 -0500

    error consulta

--- app/vistas/seguridad/perfiles/Perfiles.php
+++ app/vistas/seguridad/perfiles/Perfiles.php
@@ -12,9 +12,9 @@
 <? require '../seguridad/Menu.php'; ?>
 
 <?
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+echo "<pre>";
+print_r($_SESSION);
+echo "</pre>";
 ?>
 
 <script type="text/javascript">

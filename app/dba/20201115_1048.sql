50097656aa5954a27a2cc3c6961b69846a6303d1
JorgeAO <jaguilar.8709@gmail.com>
2020-11-17 20:46:27 -0500

    Módulo de pagos

--- app/dba/20201115_1048.sql
+++ app/dba/20201115_1048.sql
@@ -1,9 +1,9 @@
-INSERT INTO `tb_seg_dba` (`dba_codigo`, `dba_id`, `dba_ejecutada`) VALUES (NULL, '20201115_1048', '0'), 
+INSERT INTO `tb_seg_dba` (`dba_codigo`, `dba_id`, `dba_ejecutada`) VALUES (NULL, '20201115_1048', '0');
 
 ALTER TABLE `tb_pre_prestamos` 
     ADD `pres_vlr_saldo` DECIMAL(10,0) NOT NULL AFTER `pres_vlr_pago`;
 
-INSERT INTO `tb_par_estados` (`esta_codigo`, `esta_descripcion`) VALUES (NULL, 'Pagada')
+INSERT INTO `tb_par_estados` (`esta_codigo`, `esta_descripcion`) VALUES (NULL, 'Pagada');
 
 ALTER TABLE `tb_pre_cuotas` CHANGE `fk_par_estados` `fk_par_estados` INT(11) NOT NULL DEFAULT '3';
 
